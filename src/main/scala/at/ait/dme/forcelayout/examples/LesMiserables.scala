22efb73585733bd4e8f81cf70ef6f981de1b31b2
Rainer Simon <rainer.simon@ait.ac.at>
2013-06-20 14:56:06 +0200

    Customization of node/edge drawing

--- src/main/scala/at/ait/dme/forcelayout/examples/LesMiserables.scala
+++ src/main/scala/at/ait/dme/forcelayout/examples/LesMiserables.scala
@@ -6,6 +6,10 @@ import java.awt.Dimension
 import javax.swing.JFrame
 import at.ait.dme.forcelayout.{ Edge, Node, SpringGraph }
 import at.ait.dme.forcelayout.renderer.BufferedInteractiveGraphRenderer
+import java.awt.Graphics2D
+import java.awt.geom.Ellipse2D
+import java.awt.Color
+import java.awt.BasicStroke
 
 object LesMiserables extends App {
   
@@ -25,6 +29,18 @@ object LesMiserables extends App {
   val graph = new SpringGraph(nodes, edges) 
   
   val vis = new BufferedInteractiveGraphRenderer(graph)
+  vis.setNodePainter((x: Int, y: Int, n: Node, showLabels: Boolean, g2d: Graphics2D) => {
+    val size = 10
+    g2d.setColor(vis.palette(n.group % vis.palette.size))
+    g2d.fill(new Ellipse2D.Double(x - size / 2, y - size / 2, size, size))
+    g2d.setStroke(new BasicStroke(2));
+    g2d.setColor(Color.WHITE)
+    g2d.draw(new Ellipse2D.Double(x - size / 2, y - size / 2, size, size))
+    if (showLabels) {
+      g2d.setColor(Color.BLACK)
+      g2d.drawString(n.label, x + 5, y - 2)
+    } 
+  })
   
   val frame = new JFrame("Les Miserables")
   frame.setPreferredSize(new Dimension(920,720))
